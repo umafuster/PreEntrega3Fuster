135af479b0206a69867df83144633fa215a4afc5
umafuster <umafuster@gmail.com>
2023-07-10 23:00:37 -0300

    Agregado de Funciones

--- JS/main.js
+++ JS/main.js
@@ -1,29 +1,101 @@
-let capitalInicial = Number(prompt("Ingrese el capital que desea invertir"));
-if (isNaN(capitalInicial)) {
-    console.log("Ingrese un valor numérico");
-    prompt("Ingrese el capital que desea invertir");
-} else {
-    console.log("El valor ingresado es: " + capitalInicial);
+// DEFINICIONES
+
+let nombre;
+let apellido;
+let usuario = []
+let capitalInicial;
+let beneficio;
+let tiempo;
+let capitalFinal;
+
+
+
+// FUNCIONES
+
+
+// Funcion Ingreso Usuario
+function ingresoUsuario() {
+    nombre = prompt("Ingresar nombre: ");
+    apellido = prompt("Ingresar apellido: ");
+    usuario = []
+    usuario.push(nombre, apellido)
+}
+
+// Funcion Ingreso Capital
+function ingresarCapital() {
+    capitalInicial = Number(prompt("Ingrese el capital que desea invertir"));
+    if (isNaN(capitalInicial)) {
+        console.log("Ingrese un valor numérico");
+        ingresarCapital();
+    } else {
+        console.log("El valor ingresado es: " + capitalInicial);
+        usuario.push(capitalInicial)
+    }
+}
+
+// Funcion Ingreso Interes
+function tasaBeneficio() {
+    beneficio = Number(prompt("Ingrese el interes de la operacion: "))
+    usuario.push(beneficio);
+}
+
+// Funcion Ingreso Plazo de Tiempo
+function plazoTiempo() {
+    tiempo = Number(prompt("Ingrese el plazo de tiempo en meses de la operacion: "))
+    usuario.push(tiempo);
+}
+
+// Funcion Seleccionar Opcion
+function seleccionarOperacion() {
+    console.log("Elija una de las siguientes opciones");
+    console.log("1: Calcular a interés compuesto");
+    console.log("2: Calcular a interés simple");
+
+    let op = prompt("Ingrese la opcion");
+
+    switch (op) {
+        case "1":
+            compuesto();
+            break;
+        case "2":
+            simple();
+            break;
+        default:
+            console.log("Operacion invalida")
+    }
+}
+
+// Funcion Interes Compuesto
+function compuesto(){
+    beneficio = beneficio / 100;
+    capParcial = (1 + beneficio);
+    capitalFinal = capitalInicial * Math.pow(capParcial, tiempo);
+    usuario.push(capitalFinal);
+}
+
+// Funcion Interes Simple
+function simple() {
+    beneficio = beneficio / 100;
+    capitalFinal = capitalInicial * (1 + (beneficio / 100));
+    usuario.push(capitalFinal)  
 }
 
 
 
-console.log("Elija una de las siguientes opciones");
-console.log("1. Calcular a interés compuesto");
-console.log("2. Calcular a interés simple");
+// Funcion para Finalizar
+function finalizacion() {
+    console.log(usuario)
+}
 
 
-let opcionInteres = prompt("Elija la opcion");
-switch (opcionInteres) {
-    case "1":
-        console.log("Has elegido calcular a interés compuesto");
-        break;
-    case "2":
-        console.log("Has elegido calcular a interés simple");
-        break;
-    default:
-        console.log("No has elegido una opcion correcta");
-      
-} 
 
+// LLAMADOS 
 
+ingresoUsuario();
+ingresarCapital();
+tasaBeneficio();
+plazoTiempo();
+seleccionarOperacion();
+finalizacion();
+
+console.log(typeof capitalFinal);
